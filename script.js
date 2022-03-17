7b6c345337e7c56aa8c874e9b7f730de5c288015
KazaKazan <ahmetfiratusta@hotmail.com>
2022-03-17 13:21:09 +0300

    Fix a bug with the calendar day display.

--- script.js
+++ script.js
@@ -303,7 +303,7 @@ const clock = (() => {
     const clockContainer = document.getElementById("clockContainer");
 
     // Naming Vars
-    const days = ["Mon","Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    const days = ["Sun", "Mon","Tue", "Wed", "Thu", "Fri", "Sat"]
     const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
     // Private Functions
